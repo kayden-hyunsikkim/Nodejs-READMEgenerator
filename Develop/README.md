4384b8f5f55f7fd1e0b6486c7a4838dd54d31218
hyunsik kim (kayden) <kayden190116@gamil.com>
2023-02-10 19:46:43 +1030

    done till putting elements in README FILE need to design it

--- Develop/README.md
+++ Develop/README.md
@@ -1,37 +1,48 @@
-hello
+# 09 Node.js Challenge: Professional README Generator
+## Git hub user name
 
-             asdq
 
-             hello
+ [asd](https://github.com/asd)
 
-             qsdqsd
+## E-mail address
 
-             hello
 
-             sqdsd
- 
-             hello
- 
-             qsdqsd
+ eqdwe
 
-             hello
+## Project name
 
-             MIT
 
-             hello
- 
-             qsdqsd
- 
-             hello
-  
-             npm test
- 
-             hello
+ asdasd
 
-             qsdsdqs
- 
-             hello
+## Description
 
-             qsdqsd
-  
-            
+
+ sadsad
+
+## License
+
+
+ MIT ,APACHE 2.0 ,GPL 3.0 ,Eclipse Public License 1.0 
+
+ ![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)
+
+## dependencies
+
+
+ a
+
+## tests
+
+
+ npm test
+
+## Usage
+
+
+ a
+
+## Contributing
+
+
+ asd
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseBadge = {
+    MIT : '![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)',
+    APACHE : '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)',
+    Eclipse : '![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)',
+    BSD : '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)',
+    GNU : '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+    None : ''
+  }
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLink = {
+    MIT : 'https://opensource.org/licenses/MPL-2.0',
+    APACHE : 'https://opensource.org/licenses/Apache-2.0',
+    Eclipse : 'https://opensource.org/licenses/EPL-1.0',
+    BSD : 'https://opensource.org/licenses/BSD-3-Clause',
+    GNU : 'https://www.gnu.org/licenses/gpl-3.0',
+    None : ''
+  }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licensesection = {
+    MIT : '```MIT ```',
+    APACHE : '```APACHE 2.0 ```',
+    Eclipse : '```GPL 3.0 ```',
+    BSD : '```Eclipse Public License 1.0 ```)',
+    GNU : '```GNU GPL v3 ```',
+    None : ''
+  }
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+  renderLicenseBadge(license),
+  renderLicenseLink(license),
+  renderLicenseSection(license)
+
+  //return `# ${data.title}`;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown
+};
+
+
